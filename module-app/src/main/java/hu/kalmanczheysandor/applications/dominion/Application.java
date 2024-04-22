3cd44bdd5047e06e08ebb29645ed7d8201ba529c
Kálmánchey Sándor <kalmanczheysandor.com>
2024-04-22 17:42:16 +0200

    Initialisation of project structure.

---
+++ module-app/src/main/java/hu/kalmanczheysandor/applications/dominion/Application.java
@@ -0,0 +1,22 @@
+package hu.kalmanczheysandor.applications.dominion;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+        return modelMapper;
+    }
+}
