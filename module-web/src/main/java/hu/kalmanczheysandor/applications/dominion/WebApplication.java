8cf624a6795b469fd1c674f996c8c90af1a73be3
Kálmánchey Sándor <kalmanczheysandor.com>
2024-05-07 17:15:15 +0200

    Changing module name.

--- module-web/src/main/java/hu/kalmanczheysandor/applications/dominion/WebApplication.java
+++ module-web/src/main/java/hu/kalmanczheysandor/applications/dominion/WebApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class WebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(WebApplication.class, args);
     }
 
     @Bean
---
+++ module-web/src/main/java/hu/kalmanczheysandor/applications/dominion/util/ArrayUtil.java
@@ -0,0 +1,8 @@
+package hu.kalmanczheysandor.applications.dominion.util;
+
+public class ArrayUtil {
+
+    public static String[] array(String... args) {
+        return args;
+    }
+}
