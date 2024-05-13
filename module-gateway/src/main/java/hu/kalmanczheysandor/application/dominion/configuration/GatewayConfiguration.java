bb4c7066d69caecf0f5a2dc10c9d5132f83eb005
Kálmánchey Sándor <kalmanczheysandor.com>
2024-05-13 09:43:32 +0200

    [web] Bugfixing of some thymeleaf parsing error.

--- module-gateway/src/main/java/hu/kalmanczheysandor/application/dominion/configuration/GatewayConfiguration.java
+++ module-gateway/src/main/java/hu/kalmanczheysandor/application/dominion/configuration/GatewayConfiguration.java
@@ -15,6 +15,14 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.a
 
 @Configuration
 public class GatewayConfiguration {
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                      .route("ai1", r -> r.path("/ai1/**").uri("lb://ai1"))
+                      .route("web", r -> r.path("/web/**").uri("lb://web"))
+                      .build();
+    }
+
 
 //	@Bean
 //	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
@@ -39,52 +47,52 @@ public class GatewayConfiguration {
 //				.build();
 //	}
 
-    @Bean
-    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-        return builder.routes()
-                      .route("ai1", r -> r.path("/ai1/**").uri("lb://ai1"))
-
-//                      .route("web_css", r -> r.path("/web/css/custom.css").uri("http://atv.hu")) >>> https://www.atv.hu/web/css/custom.css
-//                      .route("web_css", r -> r.path("/web/css/**").uri("http://localhost:7002")) //>>> https://www.atv.hu/web/css/custom.css
-//
-//                      .route("web_css", r -> r.path("/css/**").uri("lb://web"))
-                      .route("web_js", r -> r.path("/js/**").uri("lb://web"))
-                      .route("web_image", r -> r.path("/image/**").uri("lb://web"))
-                      .route("web_font", r -> r.path("/font/**").uri("lb://web"))
-                      .route("web.logout", r -> r.path("/logout").uri("lb://web/web"))
-
-                      .route("web_css", r -> r.path("/css/**")
-                                              .filters(f -> f.filter(rewritePathFilter("/css", "/css")))
-                                              .uri("lb://web"))
-                      .route("web", r -> r.path("/web/**").uri("lb://web"))
-
-
-//                      .route("web_css", r -> r.path("/web/css/**")
-//                                                     .filters(f -> f.filter((exchange, chain) -> {
-//                                                         String originalUri = exchange.getRequest().getURI().toString();
-//                                                         String redirectUri = originalUri.replace("/web/css", "/css");
-//                                                         exchange.getResponse().setStatusCode(HttpStatus.FOUND);
-//                                                         exchange.getResponse().getHeaders().setLocation(URI.create(redirectUri));
-//                                                         return chain.filter(exchange);
-//                                                     }))
-//                                              .uri("http://localhost:7002"))
-
-
-                      .build();
-
-
+//    @Bean
+//    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
 //        return builder.routes()
-//                      .route("web", r -> r.path("/web/**").uri("lb://web"))
-//                      .build();
-
-        //                      .route("microservice_a_css", r -> r.path("/microserviceA/css/**")
-//                                                         .uri("http://microservice-a.example.com/static/css/"))
-//            .route("microservice_b_css", r -> r.path("/microserviceB/css/**")
-//                                               .uri("http://microservice-b.example.com/static/css/"))
+//                      .route("ai1", r -> r.path("/ai1/**").uri("lb://ai1"))
 //
-
-    }
-
+////                      .route("web_css", r -> r.path("/web/css/custom.css").uri("http://atv.hu")) >>> https://www.atv.hu/web/css/custom.css
+////                      .route("web_css", r -> r.path("/web/css/**").uri("http://localhost:7002")) //>>> https://www.atv.hu/web/css/custom.css
+////
+////                      .route("web_css", r -> r.path("/css/**").uri("lb://web"))
+//                      .route("web_js", r -> r.path("/js/**").uri("lb://web"))
+//                      .route("web_image", r -> r.path("/image/**").uri("lb://web"))
+//                      .route("web_font", r -> r.path("/font/**").uri("lb://web"))
+//                      .route("web.logout", r -> r.path("/logout").uri("lb://web/web"))
+//
+//                      .route("web_css", r -> r.path("/css/**")
+//                                              .filters(f -> f.filter(rewritePathFilter("/css", "/css")))
+//                                              .uri("lb://web"))
+//                      .route("web", r -> r.path("/web/**").uri("lb://web"))
+//
+//
+////                      .route("web_css", r -> r.path("/web/css/**")
+////                                                     .filters(f -> f.filter((exchange, chain) -> {
+////                                                         String originalUri = exchange.getRequest().getURI().toString();
+////                                                         String redirectUri = originalUri.replace("/web/css", "/css");
+////                                                         exchange.getResponse().setStatusCode(HttpStatus.FOUND);
+////                                                         exchange.getResponse().getHeaders().setLocation(URI.create(redirectUri));
+////                                                         return chain.filter(exchange);
+////                                                     }))
+////                                              .uri("http://localhost:7002"))
+//
+//
+//                      .build();
+//
+//
+////        return builder.routes()
+////                      .route("web", r -> r.path("/web/**").uri("lb://web"))
+////                      .build();
+//
+//        //                      .route("microservice_a_css", r -> r.path("/microserviceA/css/**")
+////                                                         .uri("http://microservice-a.example.com/static/css/"))
+////            .route("microservice_b_css", r -> r.path("/microserviceB/css/**")
+////                                               .uri("http://microservice-b.example.com/static/css/"))
+////
+//
+//    }
+//
 
     private static GatewayFilter rewritePathFilter(String sectionToReplace, String replaceWith) {
         return (exchange, chain) -> {
@@ -93,6 +101,7 @@ public class GatewayConfiguration {
             String path = req.getURI().getRawPath();
 
             String newPath = path.replaceAll(sectionToReplace, replaceWith);
+            System.out.println("ROOOTING>[" + path + "]>>>[" + newPath + "]");
 
             ServerHttpRequest request = req.mutate().path(newPath).build();
             exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, request.getURI());
