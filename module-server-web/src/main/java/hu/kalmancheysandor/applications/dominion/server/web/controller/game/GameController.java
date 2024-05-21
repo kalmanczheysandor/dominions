fae86502b17dda7e77dde8d749b70e9d3157fb8d
Kálmánchey Sándor <kalmanczheysandor.com>
2024-05-21 12:12:13 +0200

    [server-web]: Testing a simple websocket example.

--- module-server-web/src/main/java/hu/kalmancheysandor/applications/dominion/server/web/controller/game/GameController.java
+++ module-server-web/src/main/java/hu/kalmancheysandor/applications/dominion/server/web/controller/game/GameController.java
@@ -1,16 +1,22 @@
 package hu.kalmancheysandor.applications.dominion.server.web.controller.game;
 
 import hu.kalmancheysandor.application.dominion.api.ai.common.AiRequest;
-import hu.kalmancheysandor.application.dominion.service.CalculationResponse;
 import hu.kalmancheysandor.applications.dominion.server.web.configuration.mvc.AllowAjaxRequest;
 import hu.kalmancheysandor.applications.dominion.server.web.configuration.mvc.AllowStandardRequest;
 import hu.kalmancheysandor.applications.dominion.server.web.proxy.ai.Ai1ServiceProxy;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -18,6 +24,7 @@ import java.util.Set;
 @Controller
 @RequestMapping("/game")
 //@BlockAllRequestByDefault
+//@CrossOrigin(origins = "*")
 public class GameController {
   @Autowired
   private ModelMapper modelMapper;
@@ -33,25 +40,14 @@ public class GameController {
   }
 
 
-  @GetMapping("/hello")
-  @ResponseBody
-  @ResponseStatus(HttpStatus.OK)
-  @AllowAjaxRequest
-  public CalculationResponse hello() {
-    return proxy.calculate();
+  @MessageMapping("/chat")
+  @SendTo("/topic/messages")
+  public OutputMessage send(Message message) throws Exception {
+    System.out.println("HELLO: "+message);
+
+    String time = new SimpleDateFormat("HH:mm").format(new Date());
+    return new OutputMessage(message.getFrom(), message.getText(), time);
   }
-
-//
-//  @GetMapping("/attack")
-//  @ResponseBody
-//  @ResponseStatus(HttpStatus.OK)
-//  @AllowAjaxRequest
-//  public AiResponse attack() {
-//    return proxy.attack();
-//  }
-//
-
-
 
 
   @GetMapping("/attack")
---
+++ module-server-web/src/main/java/hu/kalmancheysandor/applications/dominion/server/web/controller/game/Message.java
@@ -0,0 +1,15 @@
+package hu.kalmancheysandor.applications.dominion.server.web.controller.game;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
