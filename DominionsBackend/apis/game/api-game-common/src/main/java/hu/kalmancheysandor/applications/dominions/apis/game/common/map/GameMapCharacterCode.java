c3f4309015d762db3ffdbe7eb6955f5124f155af
Kalmanchey Sandor <kalmanczheysandor@gmail.com>
2026-04-14 06:24:16 +0200

    [feature/ai-liz-agent]: Adding character code into game map representation.

---
+++ DominionsBackend/apis/game/api-game-common/src/main/java/hu/kalmancheysandor/applications/dominions/apis/game/common/map/GameMapCharacterCode.java
@@ -0,0 +1,21 @@
+package hu.kalmancheysandor.applications.dominions.apis.game.common.map;
+
+import java.io.Serializable;
+
+public enum GameMapCharacterCode implements Serializable {
+    HUMAN,
+    ODYSSEUS,
+    ACHILLES,
+    HECTOR,
+    PERSEUS,
+    THESEUS,
+    HERACLES,
+    JASON,
+    ATALANTA,
+    BELLEROPHON,
+    PATROCLUS,
+    AGAMEMNON,
+    MENELAUS,
+    DIOMEDES
+}
+
