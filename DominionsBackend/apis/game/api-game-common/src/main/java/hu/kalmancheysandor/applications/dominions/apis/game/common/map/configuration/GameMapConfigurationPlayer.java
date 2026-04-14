3a6991cc9b92d95e670c65ac69eae0d8fd3d5668
Kalmanchey Sandor <kalmanczheysandor@gmail.com>
2026-04-14 06:24:16 +0200

    [feature/ai-liz-agent]: Adding character code into game map representation.

--- DominionsBackend/apis/game/api-game-common/src/main/java/hu/kalmancheysandor/applications/dominions/apis/game/common/map/configuration/GameMapConfigurationPlayer.java
+++ DominionsBackend/apis/game/api-game-common/src/main/java/hu/kalmancheysandor/applications/dominions/apis/game/common/map/configuration/GameMapConfigurationPlayer.java
@@ -2,8 +2,10 @@ package hu.kalmancheysandor.applications.dominions.apis.game.common.map.configur
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import hu.kalmancheysandor.applications.dominions.apis.game.common.map.GameMapCharacterCode;
 import hu.kalmancheysandor.applications.dominions.apis.game.common.map.GameMapEngineType;
 import hu.kalmancheysandor.applications.dominions.apis.game.common.map.GameMapPlayerType;
+import hu.kalmancheysandor.applications.dominions.apis.game.common.map.exception.IncompatibleCharacterCodeGameMapException;
 import hu.kalmancheysandor.applications.dominions.apis.game.common.map.exception.IncompatibleEngineTypeGameMapException;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,24 +15,39 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @JsonAutoDetect(
-    fieldVisibility = JsonAutoDetect.Visibility.ANY,
-    getterVisibility = JsonAutoDetect.Visibility.NONE,
-    isGetterVisibility = JsonAutoDetect.Visibility.NONE,
-    setterVisibility = JsonAutoDetect.Visibility.NONE
+        fieldVisibility = JsonAutoDetect.Visibility.ANY,
+        getterVisibility = JsonAutoDetect.Visibility.NONE,
+        isGetterVisibility = JsonAutoDetect.Visibility.NONE,
+        setterVisibility = JsonAutoDetect.Visibility.NONE
 )
 public class GameMapConfigurationPlayer implements Serializable {
     @JsonProperty("type")
     private GameMapPlayerType type;
     @JsonProperty("engine")
     private GameMapEngineType engine;
+    @JsonProperty("character")
+    private GameMapCharacterCode character;
 
 
-    public GameMapConfigurationPlayer(GameMapPlayerType type, GameMapEngineType engine) {
+    public GameMapConfigurationPlayer(GameMapPlayerType type, GameMapEngineType engine, GameMapCharacterCode character) {
         this.type = type;
         this.engine = engine;
+        this.character = character;
 
-        if ((GameMapPlayerType.ARTIFICIAL == type && GameMapEngineType.HUMAN == engine) || (GameMapPlayerType.HUMAN == type && GameMapEngineType.HUMAN != engine)) {
-            throw new IncompatibleEngineTypeGameMapException();
+        if (GameMapPlayerType.HUMAN == type) {
+            if (GameMapEngineType.HUMAN != engine) {
+                throw new IncompatibleEngineTypeGameMapException();
+            }
+            if (GameMapCharacterCode.HUMAN != character) {
+                throw new IncompatibleCharacterCodeGameMapException();
+            }
+        } else if (GameMapPlayerType.ARTIFICIAL == type) {
+            if (GameMapEngineType.HUMAN == engine) {
+                throw new IncompatibleEngineTypeGameMapException();
+            }
+            if (GameMapCharacterCode.HUMAN == character) {
+                throw new IncompatibleCharacterCodeGameMapException();
+            }
         }
     }
 }
