2a30c9479487a9beb582c56ca0c1ae59d58d635d
Kalmanchey Sandor <kalmanczheysandor@gmail.com>
2026-04-22 13:50:32 +0200

    [tasks/features/T0007_features_ai-hugo-agent_make-it-compatible-for-current-concept]:  Implementing missing checking of safe concept deleting. | Implementing folder and file removal of relating concept. | Adding missing exception handlers.

---
+++ DominionsBackend/servers/ai/hugo/server-ai-hugo-agent/src/main/java/hu/kalmancheysandor/applications/dominions/servers/ai/hugo/agent/configuration/database/DataSourcesConfig.java
@@ -0,0 +1,39 @@
+package hu.kalmancheysandor.applications.dominions.servers.ai.hugo.agent.configuration.database;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourcesConfig {
+
+
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties("app.datasource.main")
+//    public DataSource mainDataSource() {
+//        HikariDataSource ds = DataSourceBuilder.create().type(HikariDataSource.class).build();
+//        //  ds.setTransactionIsolation("TRANSACTION_REPEATABLE_READ");
+//        return ds;
+//    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource.ai-hugo")
+    public DataSource operationDataSource() {
+        HikariDataSource ds = DataSourceBuilder.create().type(HikariDataSource.class).build();
+        return ds;
+    }
+
+//    @Bean
+//    @SpringSessionDataSource
+//    @ConfigurationProperties("app.datasource.session")
+//    public DataSource sessionDataSource() {
+//        return DataSourceBuilder.create().build();
+//    }
+}
