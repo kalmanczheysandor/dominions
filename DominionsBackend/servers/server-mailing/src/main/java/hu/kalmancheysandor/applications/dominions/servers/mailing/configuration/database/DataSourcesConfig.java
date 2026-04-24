937b6c6d3196a77c0e5f94d8767ba99f66052ee5
Kalmanchey Sandor <kalmanczheysandor@gmail.com>
2026-04-24 20:01:58 +0200

    [bugfix/B003_site_signup-errors]: Bugfix: Making image upload optional in saveUser method due to both SignUp and Settings pages use it. | Correcting links in restoration and signup emails.

---
+++ DominionsBackend/servers/server-mailing/src/main/java/hu/kalmancheysandor/applications/dominions/servers/mailing/configuration/database/DataSourcesConfig.java
@@ -0,0 +1,32 @@
+package hu.kalmancheysandor.applications.dominions.servers.mailing.configuration.database;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourcesConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("app.datasource.main")
+    public DataSource mainDataSource() {
+        HikariDataSource ds = DataSourceBuilder.create().type(HikariDataSource.class).build();
+        //  ds.setTransactionIsolation("TRANSACTION_REPEATABLE_READ");
+        return ds;
+    }
+
+//    @Bean
+//    @SpringSessionDataSource
+//    @ConfigurationProperties("app.datasource.session")
+//    public DataSource sessionDataSource() {
+//        HikariDataSource ds = DataSourceBuilder.create().type(HikariDataSource.class).build();
+//        //  ds.setTransactionIsolation("TRANSACTION_REPEATABLE_READ");
+//        return ds;
+//    }
+}
--- DominionsBackend/servers/server-mailing/src/main/java/hu/kalmancheysandor/applications/dominions/servers/mailing/service/SiteAccountMailDeliveryService.java
+++ DominionsBackend/servers/server-mailing/src/main/java/hu/kalmancheysandor/applications/dominions/servers/mailing/service/SiteAccountMailDeliveryService.java
@@ -22,7 +22,7 @@ public class SiteAccountMailDeliveryService {
 
     public void enqueueSiteAccountSignUpVerificationMail(SiteAccountSignUpVerificationMailEnqueueRequest request) {
 
-        String verificationLink = "http://localhost:8080/account/signup/verification/" + request.getVerificationToken();
+        String verificationLink = "https://game.dominions.hu/account/signup/verification/" + request.getVerificationToken();
         String htmlTemplate = """
                 <html>
                   <body style="margin:0;padding:20px;font-family:Arial,sans-serif;background-color:#f9f9f9;">
@@ -74,7 +74,7 @@ public class SiteAccountMailDeliveryService {
 
     public void enqueueSiteAccountRecoveryVerificationMail(SiteAccountRecoveryVerificationMailEnqueueRequest request) {
 
-        String verificationLink = "http://localhost:8080/account/recovery/submit/" + request.getVerificationToken();
+        String verificationLink = "https://game.dominions.hu/account/recovery/submit/" + request.getVerificationToken();
         String htmlTemplate = """
                 <html>
                   <body style="margin:0;padding:20px;font-family:Arial,sans-serif;background-color:#f9f9f9;">
