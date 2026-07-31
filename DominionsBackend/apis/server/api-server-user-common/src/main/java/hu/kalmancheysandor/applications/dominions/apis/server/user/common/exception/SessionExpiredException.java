d77287e113a2e0df4e7bdd2a8efb2eedea9a6ef7
Kalmanchey Sandor <kalmanczheysandor@gmail.com>
2026-07-31 18:14:31 +0200

    [bugfix/B006_improve-security-configuration-and-message-presentation]:Removing unnecessary permission classes.

--- DominionsBackend/apis/server/api-server-user-common/src/main/java/hu/kalmancheysandor/applications/dominions/apis/server/user/common/exception/SessionExpiredException.java
+++
@@ -1,8 +0,0 @@
-package hu.kalmancheysandor.applications.dominions.apis.server.user.common.exception;
-
-import hu.kalmancheysandor.applications.dominions.apis.server.user.common.exception.permission.ActionNotGrantedException;
-
-public class SessionExpiredException extends RuntimeException {
-    public SessionExpiredException() {
-    }
-}
